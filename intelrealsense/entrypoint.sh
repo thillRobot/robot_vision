b8b8bfec32eda8bb378ba2cd0a1447f3462cb772
thill <thill@tntech.edu>
2024-02-11 01:52:50 -0600

    testing robot_vision/intelrealsense with seam_detection

--- intelrealsense/entrypoint.sh
+++ intelrealsense/entrypoint.sh
@@ -5,16 +5,21 @@ set -e
 #export RS_WS=/home/realsense_ws
 
 export ROS=/opt/ros/noetic
-export ROS_WS=/home/catkin_ws
 source $ROS/setup.bash
+
+export ROS_WS=/home/catkin_ws
+export ROL_WS=/home/overlay_ws
+cd $ROL_WS && source $ROS_WS/devel/setup.bash && catkin_make
+
+
+#cd $ROS_WS/src/seam_detection && git pull # this wont work without keys...
+#cd $ROS_WS && catkin_make
+#cp /home/shared/launch/. -r $ROS_WS/src/realsense-ros/realsense2_camera/launch/ 
+#cp /home/shared/rviz/. -r $ROS_WS/src/realsense-ros/realsense2_camera/rviz/ 
+
 
 echo "source $ROS/setup.bash" >> /root/.bashrc
 echo "source $ROS_WS/devel/setup.bash" >> /root/.bashrc
-
-#cd $ROS_WS/src/seam_detection && git pull # this wont work without keys...
-
-cd $ROS_WS && catkin_make
-cp /home/shared/launch/. -r $ROS_WS/src/realsense-ros/realsense2_camera/launch/ 
-cp /home/shared/rviz/. -r $ROS_WS/src/realsense-ros/realsense2_camera/rviz/ 
+echo "source $ROL_WS/devel/setup.bash" >> /root/.bashrc
 
 exec "$@"
